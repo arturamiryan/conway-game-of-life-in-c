618c3cdc721f6dee873ecb3d91fd144a46d419c5
Artur Amiryan <arturamiryan0x@gmail.com>
2023-06-21 20:01:03 +0200

    Stupid bug fixed

--- source/main.c
+++ source/main.c
@@ -1,12 +1,11 @@
-#include <unistd.h>
+#include <time.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <time.h>
 
-#define WIDTH 383
-#define HEIGHT 82
+#define WIDTH 319
+#define HEIGHT 78
 
 char state[] = " #";
 bool surface[HEIGHT][WIDTH];
@@ -35,6 +34,10 @@ void compute_surface(void) {
 				  if (surface[(h1 + HEIGHT) % HEIGHT][(w1 + WIDTH) % WIDTH])
 					  cells_alive++;
 
+      if (surface[h][w]) {
+        cells_alive--;
+      }
+
       surface_diff[h][w] = (cells_alive == 3 || (cells_alive == 2 && surface[h][w]));
     }
   }
@@ -46,31 +49,27 @@ void apply_surface_diff(void)
 }
 
 void display_surface(bool surface[HEIGHT][WIDTH]) {
+  system("clear");
+  printf("\034[1;31m");
   for (size_t y = 0; y < HEIGHT; ++y) {
         for (size_t x = 0; x < WIDTH; ++x) {
             char c = state[(int)(surface[y][x])];
+            printf("\033[1;31m");
             fputc(c, stdout);
         }
-        fputc('\n', stdout);
-    }
+    fputc('\n', stdout);
+  }
+  printf("\034[0m");
 }
 
 int main(void) {
   srand(time(0));
   random_surface();
-  for(int i = 0; i < 150; ++i) {
+  for(;;) {
     display_surface(surface);
     compute_surface();
     apply_surface_diff();
-    usleep(100000);
   }
 
-  for (size_t y = 0; y < HEIGHT; ++y) {
-    for (size_t x = 0; x < WIDTH; ++x) {
-      printf("%d", surface[y][x]);
-    }
-    printf("\n");
-  }
-  
   return 0;
 }
